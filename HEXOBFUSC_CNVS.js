653545b6c671a3a44fefbf63116f7a772ace0e85
Stéphane Adam Garnier <seedsodesign@gmail.com>
2018-07-29 16:07:22 +0200

    Update HEXOBFUSC_CNVS.js

--- HEXOBFUSC_CNVS.js
+++ HEXOBFUSC_CNVS.js
@@ -1,5 +1,5 @@
-/* R: currently NOT working .. */
 var seq =["0xdd 0x5a 0x21", "0xac 0xaf 0x7b", "0xb6 0xae 0xb1", "0x0c 0x84 0x5b", "0x27 0x22 0xa2"];
+
 var hexToRgb = function(hexArr, rgbStr){
   if (rgbStr == true) return 'rgb(' + Number(hexArr[0]) + ',' + Number(hexArr[1]) + ',' + Number(hexArr[2]) + ')';
   else return {r: Number(hexArr[0]), g: Number(hexArr[1]), b: Number(hexArr[2]) };
@@ -16,58 +16,34 @@ var sentenceHexToRgb = function(sentenceHexStrArr){
   });
   return rgbsArr;
 }
-var rgbSeq = sentenceHexToRgb(seq);
 
-//var canvasOutput = function(rgbSeq){
-  //var cnvs = document.createElement('canvas');
-  var cnvs = document.querySelector('canvas');
-  var ctx = cnvs.getContext('2d');
-  cnvs.height = 1;
-  cnvs.width = rgbSeq.length; // number of 3hex chunks or r,g,b objs
-  //document.body.append(cnvs); // debug
-  var imageData = ctx.getImageData(0, 0, cnvs.width, 1); //read ( blank )
-  //var data = imageData.data;
-  var buf = anew ArrayBuffer(imageData.data.length); // tmp hold content od img data
-  var buf8 = new Uint8ClampedArray(buf); // array buffer view - representation as one-dimensional array of unsigned 8-bit values
-  //var data = new Uint32Array(buf); // array buffer view - representation as one-dimensional array of unsigned 32-bit values
-  /*
-  for(var i=0,j=0; i <data.length; i+=1,j++){
-    // set pixel at i to data in array at j
-    data[i] = 0; //rgbSeq[j].r;
-    data[i+1] = 0; //rgbSeq[j].g;
-    data[i+2] = 0; //rgbSeq[j].b;
-    console.log('looping over ' + i);
-  }
-  */
-  
-  for(var i=0,j=0; i <imageData.data.length; i+=4,j++){
-    // set pixel at i to data in array at j
-    imageData.data[i] = 255; //rgbSeq[j].r;
-    imageData.data[i+1] = 255; //rgbSeq[j].g;
-    imageData.data[i+2] = 255; //rgbSeq[j].b;
-    console.log('looping over ' + j);
-  }
-  
-  //imageData.data.set(buf8); // assign content of buffer to imageData.data - doesn't seem to work
-  //imageData.data.set(buf); // assign content of buffer to imageData.data - seems to work
-  //imageData.data.set(imageData.data); // assign content of buffer to imageData.data - seems to work
-  for(var i=0, j=0; i < imageData.data.length; i+=4, j++) {
-    console.log(j+': R:' + imageData.data[i] + ' G:' + imageData.data[i+1] +' B:' + imageData.data[i+2]);
-    imageData.data[i] = rgbSeq[j].r;
-    imageData.data[i + 1] = rgbSeq[j].g;
-    imageData.data[i + 2] = rgbSeq[j].b;
-    //console.log('looping over i: ' + i + ' and j: ' + j);
-    console.log(j+': R:' + imageData.data[i] + ' G:' + imageData.data[i+1] +' B:' + imageData.data[i+2]);
-  }
-  //imageData.data = data; // necessary ?
-  ctx.putImageData(imageData, 0, 0); // write imageData back to canvas
-  
-  // == debug: re-read image data from canvas ==
-  var imageData2 = ctx.getImageData(0, 0, cnvs.width, 1); //read ( blank )
-  var data2 = imageData2.data;
-  for(var i=0, j=0; i < data2.length; i+=4, j++) {
-    console.log(j+': R:' + data2[i] + ' G:' + data2[i+1] +' B:' + data2[i+2]);
-  }
-//}
+var hexToImage = function(seq){
+  var rgbSeq = sentenceHexToRgb(seq);
 
-//canvasOutput(rgbSeq);
+  // == canvas part ==
+  //var canvas = document.querySelector('canvas');
+  var canvas = document.createElement('canvas'); // offscreen canvas
+  canvas.height = 1;
+  canvas.width = rgbSeq.length; // number of 3hex chunks or r,g,b objs
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'white'; // mandatory !
+  ctx.fillRect(0, 0, canvas.width, canvas.height); // mandatory !
+  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+  for(var i=0, j=0; i < data.length; i+=4, j++) {
+    data[i] = rgbSeq[j].r;
+    data[i + 1] = rgbSeq[j].g;
+    data[i + 2] = rgbSeq[j].b;
+  }
+  ctx.putImageData(imageData, 0, 0);
+  // saving the canvas
+  var canvasImg = canvas.toDataURL();
+  var newdata = canvasImg.replace(/^data:image\/png/,'data:application/octet-stream');
+  //window.open( newdata ); // doesn't allow us to specify a filename
+  var link = document.createElement('a');
+  link.setAttribute('download', 'output.png');
+  link.setAttribute('href', newdata);
+  link.click();
+}
+
+//hexToImage(seq);
